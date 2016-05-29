c01c4be61518af2191de2cb856147be05b6893de
dima <zelinski.dima@gmail.com>
2016-05-29 19:50:20 +0300

    Writed classes for User and Message entities

---
+++ Chat/Entities/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entities
+{
+    [Serializable]
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public User Author { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Chat/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entities
+{
+    [Serializable]
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Nickname { get; set; }
+    }
+}

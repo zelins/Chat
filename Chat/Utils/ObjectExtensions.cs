f4151339831fbc1e5e0e78919869ba00381782bd
dima <zelinski.dima@gmail.com>
2016-05-29 13:04:31 +0300

    Writed extension methods to serialize/deserialize objects

---
+++ Chat/Utils/ObjectExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Utils
+{
+    public static class ObjectExtensions
+    {
+        public static byte[] SerializeToByteArray(this object obj)
+        {
+            var formatter = new BinaryFormatter();
+
+            byte[] bytes;
+
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, obj);
+
+                bytes = stream.ToArray();
+            }
+
+            return bytes;
+        }
+
+        public static object DeserializeToObject(this byte[] bytes)
+        {
+            var formatter = new BinaryFormatter();
+
+            var stream = new MemoryStream(bytes);
+
+            return formatter.Deserialize(stream);
+        }
+    }
+}

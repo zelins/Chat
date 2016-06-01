de8053a209c159a27bfb4408393dd2c7ace76413
Zelinski Dima <zelinski.dima@gmail.com>
2016-06-01 16:38:36 +0300

    Added client class on server side (to representing client's important properties)

---
+++ Chat/Server/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Net.Sockets;
+using Commands.Abstracts;
+using Entities;
+
+namespace Server
+{
+    public class Client
+    {
+        public ImmutableQueue<IChatCommand> CommandsQueue { get; set; }
+
+        public TcpClient TcpClient { get; }
+
+        public User User { get; }
+
+        public NetworkStream Stream => this.TcpClient.GetStream();
+
+        public Client(User user, TcpClient client)
+        {
+            this.TcpClient = client;
+            this.CommandsQueue = ImmutableQueue<IChatCommand>.Empty;
+            User = user;
+        }
+    }
+}

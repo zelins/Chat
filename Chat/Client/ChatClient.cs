36771b5b519479061abb7eecaf06a457a5d41f64
Zelinski Dima <zelinski.dima@gmail.com>
2016-06-01 17:29:52 +0300

    Added base class to Client (BindableBase) and refactore client class

--- Chat/Client/ChatClient.cs
+++ Chat/Client/ChatClient.cs
@@ -1,22 +1,32 @@
-﻿using System;
-using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Net.Sockets;
-using System.Threading.Tasks;
-using Commands;
+﻿using Commands;
 using Commands.Abstracts;
 using Entities;
+using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Immutable;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using Utils;
 
 namespace Client
 {
-    public class ChatClient : IDisposable
+    public class ChatClient : BindableBase, IDisposable
     {
+        #region Fields
+
         private TcpClient client = new TcpClient();
 
         private ImmutableQueue<IChatCommand> commandsQueue = ImmutableQueue<IChatCommand>.Empty;
 
+        #endregion Fields
+
+        #region Properties
+
         public User User { get; private set; }
+
+        #endregion Properties
+
+        #region Methods
 
         public async void ConnectToServer(string name)
         {
@@ -27,30 +37,15 @@ namespace Client
                 Nickname = name
             };
             IChatCommand command = new ConnectCommand
-            (
+                (
                 User,
                 DateTime.Now,
                 true
-            );
+                );
 
             this.commandsQueue = this.commandsQueue.Enqueue(command);
 
             HandleCommands();
-        }
-
-        public void EnqueueMessage(string message)
-        {
-            var messageCommand = new MessageCommand
-            (
-                new Message
-                {
-                    Author = User,
-                    Id = Guid.Empty,
-                    Content = message
-                },
-                DateTime.Now
-            );
-            this.commandsQueue = this.commandsQueue.Enqueue(messageCommand);
         }
 
         private async void HandleCommands()
@@ -84,11 +79,32 @@ namespace Client
             }
         }
 
+        public void EnqueueMessage(string message)
+        {
+            var messageCommand = new MessageCommand
+                (
+                new Message
+                {
+                    Author = User,
+                    Id = Guid.Empty,
+                    Content = message
+                },
+                DateTime.Now
+                );
+            this.commandsQueue = this.commandsQueue.Enqueue(messageCommand);
+        }
+
+        #endregion Methods
+
+        #region IDisposable implementation
+
         /// <summary>
         /// Will be implemented by Fody weaving
         /// </summary>
         public void Dispose()
         {
         }
+
+        #endregion IDisposable implementation
     }
 }
